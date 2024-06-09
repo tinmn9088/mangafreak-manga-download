df3dc18ffb3669a0dee8bf9f5f5cff7d0fb5ea0e
tinmn9088@darwin <tinman.8.9@yandex.ru>
2024-06-09 18:55:57 +0300

    chore: extract downloaded chapters

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import os
+import zipfile
+from pathlib import Path
 from cli import ArgumentService
 from mangafreak import MangafreakService
 
@@ -26,6 +28,33 @@ print(f'Files downloaded: {len(downloaded_file_paths)}')
 
 downloads_path = os.path.commonpath(downloaded_file_paths)
 
-print(sorted(os.listdir(downloads_path)))
+# prepare extraction directory
+extraction_common_path = os.path.join(downloads_path, 'extracted')
+os.makedirs(extraction_common_path, exist_ok=True)
+
+# iterate through downloaded archives and extract images
+archive_number = 0
+
+for archive_name in sorted(os.listdir(downloads_path)):
+
+    # get archive full path
+    archive_path = os.path.join(downloads_path, archive_name)
+
+    print(f'Extracting {archive_path}:')
+
+    # prepare extraction directory for this specific archive
+    extraction_path = os.path.join(
+        extraction_common_path,
+        f'{(archive_number // settings.chapters_per_file + 1):03d}',  # group archives
+        Path(archive_name).stem
+    )
+
+    os.makedirs(extraction_path, exist_ok=True)
+
+    # extract archive
+    with zipfile.ZipFile(archive_path, 'r') as archive_file:
+        archive_file.extractall(extraction_path)
+
+    archive_number += 1
 
 # TODO: assemble images in PDF
