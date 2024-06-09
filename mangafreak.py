1e9cf333412e15481641007fd4b7625fc82306d3
tinmn9088@darwin <tinman.8.9@yandex.ru>
2024-06-09 10:49:50 +0300

    chore: implement `download_chapter` method in `MangafreakService`

--- mangafreak.py
+++ mangafreak.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import re
+import requests
+import os
 
 
 class MangafreakService:
@@ -7,6 +9,10 @@ class MangafreakService:
     TITLE_SELECTOR = '.manga_series_data h1'
 
     CHAPTER_LIST_SELECTOR = '.manga_series_list_section tbody'
+
+    DOWNLOADS_DIRECTORY = 'downloads'
+
+    DOWNLOAD_CHUNK_SIZE_BYTES = 1024 * 1024
 
     @classmethod
     def parse_html(cls, chapter_list_html_path: str) -> tuple[str, list[str]]:
@@ -56,3 +62,48 @@ class MangafreakService:
         no_whitespaces = re.sub('\s+', '_', only_word_characters)
 
         return f'{no_whitespaces.lower()}'
+
+    @staticmethod
+    def get_chapter_download_link(manga_title_url_version: str, chapter_number: str) -> str:
+        return f'https://images.mangafreak.net/downloads/{manga_title_url_version}_{chapter_number}'
+
+    @classmethod
+    def download_chapter(cls, manga_title_url_version: str, chapter_number: str) -> str:
+        '''
+        Download chapter (zip-archive with JPG-images) and get output file path.
+        '''
+
+        # generate download link
+        link = cls.get_chapter_download_link(manga_title_url_version, chapter_number)
+
+        # create required directories
+        base_path = os.path.join(os.getcwd(), cls.DOWNLOADS_DIRECTORY, manga_title_url_version)
+        os.makedirs(base_path, exist_ok=True)
+
+        # generate output file path
+        output_file_path = os.path.join(base_path, f'{chapter_number}.zip')
+
+        # open file for writing and make HTTP-request
+        with open(output_file_path, 'wb') as output_file, requests.get(link, stream=True) as response:
+
+            print(f'Downloading from {link}:')
+
+            # hide cursor
+            print('\033[?25l', end='')
+
+            chunk_number = 1
+
+            for chunk in response.iter_content(chunk_size=cls.DOWNLOAD_CHUNK_SIZE_BYTES):
+
+                output_file.write(chunk)
+
+                print(f'\rDownloaded: {os.path.getsize(output_file_path)} bytes', end='')
+
+                chunk_number += 1
+
+            # show cursor
+            print('\033[?25h')
+
+            print(f'Chapter {chapter_number} saved to {output_file_path}')
+
+        return output_file_path
