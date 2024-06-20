1a0755923c834a8f2e282b6e753ac2f41a421ae8
tinmn9088@RO-SD22 <tinman.8.9@yandex.ru>
2024-06-20 12:49:50 +0300

    refactor: run.py

--- run.py
+++ run.py
@@ -5,36 +5,41 @@ from files import FileService
 from imagemagick import ImageMagickService
 
 
-settings = ArgumentService.get_settings()
+def main() -> None:
+    settings = ArgumentService.get_settings()
 
-print('Running with settings:', vars(settings))
+    print('Running with settings:', vars(settings))
 
-# parse HTML
-title, chapter_numbers = MangafreakService.parse_html(settings)
+    # parse HTML
+    title, chapter_numbers = MangafreakService.parse_html(settings)
 
-# transform title to its url version
-title_url_version = MangafreakService.get_title_url_version(title)
+    # transform title to its url version
+    title_url_version = MangafreakService.get_title_url_version(title)
 
-# download chapters (zip-archives)
-downloaded_file_paths = []
+    # download chapters (zip-archives)
+    downloaded_file_paths = []
 
-for chapter_number in chapter_numbers:
+    for chapter_number in chapter_numbers:
 
-    downloaded_file_path = MangafreakService.download_chapter(title_url_version, chapter_number)
+        downloaded_file_path = MangafreakService.download_chapter(title_url_version, chapter_number)
 
-    downloaded_file_paths.append(downloaded_file_path)
+        downloaded_file_paths.append(downloaded_file_path)
 
-print(f'Archives ready: {len(downloaded_file_paths)}')
+    print(f'Archives ready: {len(downloaded_file_paths)}')
 
-# extract downloaded archives
-downloads_path = os.path.commonpath(downloaded_file_paths)
+    # extract downloaded archives
+    downloads_path = os.path.commonpath(downloaded_file_paths)
 
-extraction_paths = FileService.extract_archives(downloads_path, settings)
+    extraction_paths = FileService.extract_archives(downloads_path, settings)
 
-# prepare images
-image_directory_paths = FileService.prepare_images(extraction_paths)
+    # prepare images
+    image_directory_paths = FileService.prepare_images(extraction_paths)
 
-# assemble images in PDFs
-pdf_paths = ImageMagickService.convert_images_to_pdf(downloads_path, title_url_version, image_directory_paths, settings)
+    # assemble images in PDFs
+    pdf_paths = ImageMagickService.convert_images_to_pdf(downloads_path, title_url_version, image_directory_paths, settings)
 
-print(f'PDFs created: {len(pdf_paths)} (see {os.path.commonpath(pdf_paths)})')
+    print(f'PDFs created: {len(pdf_paths)} (see {os.path.commonpath(pdf_paths)})')
+
+
+if __name__ == "__main__":
+    main()
